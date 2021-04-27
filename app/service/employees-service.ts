a34c50b2a53e4786c0a8796eba18aedef4aed1e3
ezequias <ezequiassam@hotmail.com>
2021-04-26 22:50:00 -0300

    Add service

---
+++ app/service/employees-service.ts
@@ -0,0 +1,65 @@
+import {Model} from 'mongoose';
+import {EmployeeModel} from "../model";
+
+export class EmployeesService {
+  private employee: Model<any>;
+
+  constructor(employee: Model<any>) {
+    this.employee = employee;
+  }
+
+  /**
+   * Create employee
+   * @param params
+   */
+  protected async createEmployee(params: EmployeeModel): Promise<object> {
+    try {
+      const result = await this.employee.create({
+        name: params.name,
+        id: params.id,
+      });
+
+      return result;
+    } catch (err) {
+      console.error(err);
+
+      throw err;
+    }
+  }
+
+  /**
+   * Update a employee by id
+   * @param id
+   * @param data
+   */
+  protected updateEmployee(id: number, data: object) {
+    return this.employee.findOneAndUpdate(
+      {id},
+      {$set: data},
+      {new: true},
+    );
+  }
+
+  /**
+   * Find employee
+   */
+  protected findEmployee() {
+    return this.employee.find();
+  }
+
+  /**
+   * Query employee by id
+   * @param id
+   */
+  protected findOneEmployeeById(id: number) {
+    return this.employee.findOne({id});
+  }
+
+  /**
+   * Delete employee by id
+   * @param id
+   */
+  protected deleteOneEmployeeById(id: number) {
+    return this.employee.deleteOne({id});
+  }
+}
